71a410f3590064648c49a83e92fb28c07d77b400
darshanbajeja <darshanbajeja12345@gmail.com>
2021-01-14 15:12:15 +0530

    Add input from file

--- CalebHandsOnJava/src/HandsOn.java
+++ CalebHandsOnJava/src/HandsOn.java
@@ -1,8 +1,26 @@
-//import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HandsOn {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		Scanner in = new Scanner(new File("test.txt"));
+		
+		List<String> stds = new ArrayList<String>();
+		
+		while(in.hasNextLine()) {
+			stds.add(in.nextLine());
+		}
+		
+		for (int i = 0; i < stds.size(); i++) {
+			System.out.println("name: " + stds.get(i));
+		}
+
+		in.close();
 		
 		
 	}
